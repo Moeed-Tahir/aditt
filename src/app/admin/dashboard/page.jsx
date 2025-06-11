de1800762a3d2fb70fbfb90ec9f0d2b8b135e8f4
abdoo <abdullah.k10204@gmail.com>
2025-06-11 14:12:10 +0500

    started working on admin dashboard

---
+++ src/app/admin/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      I am admin dashboard
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/signin-admin/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      I am admin login
+    </div>
+  )
+}
+
+export default page
