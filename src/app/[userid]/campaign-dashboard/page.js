9b36a68ce0730b5b18c7a90389bdddc81a0da21c
abdoo <abdullah.k10204@gmail.com>
2025-06-23 15:22:31 +0500

    minor fix

--- src/app/[userid]/campaign-dashboard/page.js
+++ src/app/[userid]/campaign-dashboard/page.js
@@ -2,32 +2,30 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSidebar";
 import { DataTable } from "@/components/DataTable";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { toast } from "sonner";
 
 export default function CampaignDashboard() {
-  const [campaignData, setCampaignData] = useState()
+  const [campaignData, setCampaignData] = useState();
 
+  const fetchCampaign = useCallback(async () => {
+    try {
+      const userId = Cookies.get("userId");
+      const response = await axios.post("/api/routes/v1/campaignRoutes?action=getCampaign", {
+        userId: userId
+      });
+      setCampaignData(response.data.campaign);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Error is occur");
+      console.error('Error creating campaign:', error);
+    }
+  }, []);
 
   useEffect(() => {
-    const userId = Cookies.get("userId");
-    const fetchCampaign = async () => {
-      try {
-
-        const response = await axios.post("/api/routes/v1/campaignRoutes?action=getCampaign", {
-          userId: userId
-        });
-
-        setCampaignData(response.data.campaign);
-      } catch (error) {
-        toast.error(error?.response?.data?.message || "Error is occur");
-        console.error('Error creating campaign:', error);
-      }
-    };
     fetchCampaign();
-  }, [])
+  }, [fetchCampaign]);
 
   return (
     <SidebarProvider>
